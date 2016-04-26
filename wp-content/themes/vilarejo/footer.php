57b94c5e9c65a8859bbed6c75e3d8cbeece150a0
Gaston Briones <gbriones@cmd.com.ar>
2016-04-26 01:53:23 -0300

    fix

--- wp-content/themes/vilarejo/footer.php
+++ wp-content/themes/vilarejo/footer.php
@@ -1,4 +1,4 @@
-<footer id="footer" class="wrapper">
+<footer id="footer">
     <?php wp_footer(); ?>
     <link href="<?php echo get_template_directory_uri(); ?>/css/bxslider.css" rel="stylesheet" type="text/css">
     <script src="<?php echo get_template_directory_uri(); ?>/js/init.js"></script>
--- wp-content/themes/vilarejo/index.php
+++ wp-content/themes/vilarejo/index.php
@@ -1,9 +1,17 @@
 <?php
 get_header();
 
-$page_comfort = query_posts('page_id=9');
-$page_visit = query_posts('page_id=27');
-$page_localization = query_posts('page_id=12');
+$page_comfort =  new WP_Query('page_id=9');
+$page_visit = new WP_Query('page_id=27');
+$page_localization = new WP_Query('page_id=12');
+
+$args = array(
+    'cat' => 3,
+    'order' => 'ASC',
+    'showposts' => '1'
+);
+
+$chale = new WP_Query($args);
 
 ?>
 <body <?php body_class(); ?>>
@@ -12,17 +20,110 @@ $page_localization = query_posts('page_id=12');
     <div class="carousel">
         <?php if( function_exists('bxslider') ) bxslider('slider-home'); ?>
     </div>
-    <div class="grid-spaceAround">
-        <div class="col-3">
-            <?php if ($query->have_posts()):
-                while ($query->have_posts()):$query->the_post();?>
-                    <h4 class="page-title  wow bounceInDown">
-                        <?php the_title(); ?>
-                    </h4>
-                <?php endwhile; endif; ?>
+    <div class="wrapper">
+        <div class="grid-center main-menu">
+            <div class="col-12 grid-spaceAround">
+                <div class="col-3 item">
+                    <?php if ($page_comfort->have_posts()):
+                        while ($page_comfort->have_posts()):$page_comfort->the_post();?>
+                            <figure>
+                                <a href="<?php the_permalink() ?>">
+                                    <img src="<?php echo the_field('page_image') ?>"/>
+                                </a>
+                            </figure>
+                            <a href="<?php the_permalink() ?>">
+                                <h2 class="page-title acomodacoes">
+                                    <?php the_title(); ?>
+                                </h2>
+                                <div class="entry">
+                                    <?php echo the_field('page_subtitle') ?>
+                                </div>
+                            </a>
+                        <?php endwhile; endif; ?>
+                </div>
+                <div class="col-3 item">
+                    <?php if ($page_visit->have_posts()):
+                        while ($page_visit->have_posts()):$page_visit->the_post(); ?>
+                            <figure>
+                                <a href="<?php the_permalink() ?>">
+                                    <img src="<?php echo the_field('page_image') ?>"/>
+                                </a>
+                            </figure>
+                            <a href="<?php the_permalink() ?>">
+                                <h2 class="page-title visite">
+                                    <?php the_title(); ?>
+                                </h2>
+                                <div class="entry">
+                                    <?php echo the_field('page_subtitle') ?>
+                                </div>
+                            </a>
+                        <?php endwhile; endif; ?>
+                </div>
+                <div class="col-3 item ">
+                    <?php if ($page_localization->have_posts()):
+                        while ($page_localization->have_posts()):$page_localization->the_post();?>
+                            <figure>
+                                <a href="<?php the_permalink() ?>">
+                                    <img src="<?php echo the_field('page_image') ?>"/>
+                                </a>
+                            </figure>
+                            <a href="<?php the_permalink() ?>">
+                                <h2 class="page-title localizacao">
+                                    <?php the_title(); ?>
+                                </h2>
+                                <div class="entry">
+                                    <?php echo the_field('page_subtitle') ?>
+                                </div>
+                            </a>
+                        <?php endwhile; endif; ?>
+                </div>
+            </div>
         </div>
-        <div class="col-3">2</div>
-        <div class="col-3">3</div>
+        <div class="grid-center extras">
+            <div class="col-9">
+                <div class="grid-spaceAround block comodidades">
+                    <?php if ($chale->have_posts()):
+                        while ($chale->have_posts()):$chale->the_post();?>
+                            <div class="col-5">
+                                <figure>
+                                    <a href="<?php the_permalink() ?>">
+                                        <img src="<?php echo the_field('chale_image_1') ?>"/>
+                                    </a>
+                                </figure>
+                            </div>
+                            <div class="col-6">
+                                <h2 class="title">Comodidades do Chalés</h2>
+                                <img src="<?php echo get_template_directory_uri(); ?>/images/comodidades-chale.png">
+                                <div class="list">
+                                    <?php echo the_field('comodidades') ?>
+                                </div>
+                                <a href="<?php the_permalink() ?>" class="more">MAIS INFORMAÇÃO</a>
+                            </div>
+                        <?php endwhile; endif; ?>
+                </div>
+                <div class="grid-spaceAround block visite">
+                    <?php if ($page_visit->have_posts()):
+                        while ($page_visit->have_posts()):$page_visit->the_post();?>
+                            <div class="col-5">
+                                <figure>
+                                    <a href="<?php the_permalink() ?>">
+                                        <img src="<?php echo the_field('page_image') ?>"/>
+                                    </a>
+                                </figure>
+                            </div>
+                            <div class="col-6">
+                                <h2 class="title">Fique no coração de Maresias</h2>
+
+                                <div class="list">
+                                    <?php echo the_content() ?>
+                                </div>
+                                <a href="<?php the_permalink() ?>" class="more">MAIS INFORMAÇÃO</a>
+                            </div>
+                        <?php endwhile; endif; ?>
+                </div>
+            </div>
+            <div class="col-3 facebook" ></div>
+        </div>
     </div>
 </section>
 <?php get_footer(); ?>
